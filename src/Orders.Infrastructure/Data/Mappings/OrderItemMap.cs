c93035a138d4911cfc8e195f851bd29bf6505108
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-28 15:28:30 -0300

    feat(infrastructure/data): connect PostgresSql and run migrations

---
+++ src/Orders.Infrastructure/Data/Mappings/OrderItemMap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Infrastructure.Data.Mappings
+{
+    public class OrderItemMap : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(i => i.ProductId)
+                .IsRequired();
+
+            builder.Property(i => i.ProductName)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(i => i.UnitPrice)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(i => i.Quantity)
+                .IsRequired();
+
+            builder.Property(i => i.CreatedAt)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Orders.Infrastructure/Data/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Infrastructure.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id) =>
+            await _dbSet.FindAsync(id);
+
+        public async Task<IEnumerable<T>> GetAllAsync() =>
+            await _dbSet.ToListAsync();
+
+        public async Task AddAsync(T entity) =>
+            await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) =>
+            _dbSet.Update(entity);
+
+        public void Remove(T entity) =>
+            _dbSet.Remove(entity);
+    }
+}
