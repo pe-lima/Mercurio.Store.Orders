3ba3ed82aca7c49906c1f9cfb53cbd72dd216c15
Pedro Lima <pedro.limma.dev@gmail.com>
2025-04-28 15:29:11 -0300

    feat(api): Create a api to call the services

---
+++ src/Orders.Api/Program.cs
@@ -0,0 +1,48 @@
+using Orders.IoC;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+// Swagger configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Orders API",
+        Version = "v1",
+        Description = "API RESTful para gerenciamento de pedidos"
+    });
+});
+
+// Register project dependencies
+builder.Services.AddDependencies(builder.Configuration);
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
